69103c782442ec88f26cc47b525384cdd24c1387
Davin Lim <davin.lim14@gmail.com>
2023-05-24 11:19:59 +0700

    Update Davin.java

---
+++ fashionStore.java/src/Davin.java
@@ -0,0 +1,11 @@
+//Davin Lim
+//03081220036
+
+public class Davin{
+    public static void main(String[] args) {
+        System.out.println("Perulangan 1 sampai 5");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Perulangan ke-" + (i));
+        }
+    }
+}
