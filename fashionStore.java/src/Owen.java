d5a12e8f18fb3df822441eaa69c7bf321d269cbe
owen <itsowenlu@gmail.com>
2023-05-24 11:14:22 +0700

    Insert Owen.Java
    Insert Java File with Loop

---
+++ fashionStore.java/src/Owen.java
@@ -0,0 +1,13 @@
+// Owen
+// 03081220045
+
+public class Owen {
+
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 100) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
