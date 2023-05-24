3737dfbeefd2cb153ba04ff19174c25a8d0050d3
lolkimbek <134467017+lolkimbek@users.noreply.github.com>
2023-05-24 11:15:01 +0700

    for
    membuat perulangan

---
+++ fashionStore.java/src/JUSTIN.java
@@ -0,0 +1,10 @@
+//JUSTIN
+//03081220033
+//program perulangan
+public class JUSTIN{
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Perulangan ke " + (i + 1));
+        }
+    }
+}
