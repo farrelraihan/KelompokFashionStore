d1179c68fa05253508a67237e8ec6b8bdfa80df9
lolkimbek <134467017+lolkimbek@users.noreply.github.com>
2023-05-29 10:41:00 +0700

    deskripsi order
    deskripsi

--- fashionStore.java/src/Model/Order.java
+++ fashionStore.java/src/Model/Order.java
@@ -1,5 +1,16 @@
 package Model;
 
 public class Order {
-    
+
+    /*
+     * deskripsi
+     * nama            : Untuk menyimpan nama pelanggan
+     * ID Produk       : Untuk mentimpan kode produk  yang di order
+     * Stok            : Jumlah stok tersisa dari produk ytang di pesan 
+     * Tanggal Pesanan : Tanggal ketika pesanan di pesan pelanggan
+     * Harga           : harga dari produk yang di pesan
+     * Jumlah          : jumlah produk yang dipesan
+     * Harga           : harga produk yang dipesan oleh pelanggan 
+     * 
+     */
 }
