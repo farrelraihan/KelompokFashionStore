4b05e092befdcb497ba8a54d1e1152907db6e21d
Davin Lim <davin.lim14@gmail.com>
2023-05-29 10:15:41 +0700

    Update Costumer.java

---
+++ fashionStore.java/src/model/Costumer.java
@@ -0,0 +1,15 @@
+package model;
+/*
+ * Name : Davin Lim
+ */
+
+public class Costumer {
+    /*
+     * Deskripsi :
+     * name : untuk menyimpan nama pelanggan
+     * phoneNumber : untuk menyimpan nomor telepon pelanggan
+     * email : untuk menyimpan email pelanggan
+     * address : untuk menyimpan alamat tempat tinggal pelanggan
+     */
+    
+}
