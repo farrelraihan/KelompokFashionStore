fb9d7b75be74a127d5eae9be64101fbebd6f9c08
farrelraihan <113604965+farrelraihan@users.noreply.github.com>
2023-05-29 09:57:47 +0700

    Update App.java

--- fashionStore.java/src/App.java
+++ fashionStore.java/src/App.java
@@ -2,5 +2,10 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("Ini adalah kelompok fashion store ASHIAAAAP");
+
+
     }
+    //disini code menu tampilan awal
+
+    
 }
