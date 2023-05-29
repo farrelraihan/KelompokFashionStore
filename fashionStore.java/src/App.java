bd0dff07e10b5ce55d8a01d4ceb9289c53d3cc27
Xantaz8 <saliim.artt@gmail.com>
2023-05-29 10:27:26 +0700

    tambah
    tambah

--- fashionStore.java/src/App.java
+++ fashionStore.java/src/App.java
@@ -44,6 +44,11 @@ public class App {
         
     //aku buat class costumer - davin
     //order-Justin
+<<<<<<< Updated upstream
     // karyawan-salim
+=======
+    //karyawan - salim
+    //
+>>>>>>> Stashed changes
 
     
