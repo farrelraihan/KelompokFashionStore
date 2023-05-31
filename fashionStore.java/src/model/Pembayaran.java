3bd12ff84a93920eb0acac37ac646b28a920b56a
lolkimbek <134467017+lolkimbek@users.noreply.github.com>
2023-05-31 10:46:06 +0700

    Create Pembayaran.java

---
+++ fashionStore.java/src/model/Pembayaran.java
@@ -0,0 +1,16 @@
+package model;
+/*
+ * Name : Owen
+ */
+public class Pembayaran {
+    /*
+     * Deskripsi :
+     * Amount: Jumlah untuk yang dibayar
+     * Currency: Tipe Mata Uang
+     * Method : Metode Pembayaran
+     * Transaction ID : ID transaksi
+     * Date : Tanggal Transaksi Pembayaran
+     * TIme : Waktu Transaksi Pembayaran
+     */
+    
+}
