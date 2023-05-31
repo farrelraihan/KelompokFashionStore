b6680c079ae3dc9d60f9eb820625d5e76d3a9874
Xantaz8 <saliim.artt@gmail.com>
2023-05-31 11:05:31 +0700

    Update karyawan.java
    update

--- fashionStore.java/src/Model/karyawan.java
+++ fashionStore.java/src/Model/karyawan.java
@@ -10,5 +10,58 @@ public class karyawan {
      * NomorTelepon : untuk menginput nomor telepon karyawan
      */
 
-    
+    String idKaryawan;
+    String Name;
+    String Jabatan;
+    String Departemen;
+    int Gaji;
+    String NomorTelepon;
+
+    public String getIdKaryawan() {
+        return this.idKaryawan;
+    }
+
+    public void setIdKaryawan(String idKaryawan) {
+        this.idKaryawan = idKaryawan;
+    }
+
+    public String getName() {
+        return this.Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public String getJabatan() {
+        return this.Jabatan;
+    }
+
+    public void setJabatan(String Jabatan) {
+        this.Jabatan = Jabatan;
+    }
+
+    public String getDepartemen() {
+        return this.Departemen;
+    }
+
+    public void setDepartemen(String Departemen) {
+        this.Departemen = Departemen;
+    }
+
+    public int getGaji() {
+        return this.Gaji;
+    }
+
+    public void setGaji(int Gaji) {
+        this.Gaji = Gaji;
+    }
+
+    public String getNomorTelepon() {
+        return this.NomorTelepon;
+    }
+
+    public void setNomorTelepon(String NomorTelepon) {
+        this.NomorTelepon = NomorTelepon;
+    }
 }
