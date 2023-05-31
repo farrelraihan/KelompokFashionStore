9d808cddf48bed849e9d43ea1d15af3d610ebd31
farrelraihan <113604965+farrelraihan@users.noreply.github.com>
2023-05-31 11:02:43 +0700

    Update Produk.java
    Penambahan Attributes dan Centre Getter

--- fashionStore.java/src/Model/Produk.java
+++ fashionStore.java/src/Model/Produk.java
@@ -21,5 +21,80 @@ public class Produk {
      
 
      */
+    String merk;
+    String nama;
+    String idProduk;
+    String kategori;
+    Integer harga;
+    Integer stok;
+    String warna;
+    String ukuran;
 
+    public String getMerk() {
+        return this.merk;
+    }
+
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+
+    public String getNama() {
+        return this.nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getIdProduk() {
+        return this.idProduk;
+    }
+
+    public void setIdProduk(String idProduk) {
+        this.idProduk = idProduk;
+    }
+
+    public String getKategori() {
+        return this.kategori;
+    }
+
+    public void setKategori(String kategori) {
+        this.kategori = kategori;
+    }
+
+    public Integer getHarga() {
+        return this.harga;
+    }
+
+    public void setHarga(Integer harga) {
+        this.harga = harga;
+    }
+
+    public Integer getStok() {
+        return this.stok;
+    }
+
+    public void setStok(Integer stok) {
+        this.stok = stok;
+    }
+
+    public String getWarna() {
+        return this.warna;
+    }
+
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+
+    public String getUkuran() {
+        return this.ukuran;
+    }
+
+    public void setUkuran(String ukuran) {
+        this.ukuran = ukuran;
+    }
+
+
+
+    
 }
