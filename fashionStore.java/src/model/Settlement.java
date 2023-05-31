77ace6cd9aa573d6c0e640e28426fd53d6256b45
owen <itsowenlu@gmail.com>
2023-05-31 11:08:01 +0700

    Push Setter Getter & Attribut Settlement.java

--- fashionStore.java/src/model/Settlement.java
+++ fashionStore.java/src/model/Settlement.java
@@ -1,4 +1,5 @@
 package model;
+
 /*
  * Name : Owen
  */
@@ -9,6 +10,54 @@ public class Settlement {
      * Date : Tanggal Transaksi Pembayaran
      * Time : Waktu Transaksi Pembayaran
      * Status : Status Transaksi Pembayaran
+     * 
      */
-    
+
+    String idTransaksi;
+    String metode;
+    Integer tanggalSettlement;
+    Integer waktuSettlement;
+    String status;
+
+
+    public String getIdTransaksi() {
+        return this.idTransaksi;
+    }
+
+    public void setIdTransaksi(String idTransaksi) {
+        this.idTransaksi = idTransaksi;
+    }
+
+    public String getMetode() {
+        return this.metode;
+    }
+
+    public void setMetode(String metode) {
+        this.metode = metode;
+    }
+
+    public Integer getTanggalSettlement() {
+        return this.tanggalSettlement;
+    }
+
+    public void setTanggalSettlement(Integer tanggalSettlement) {
+        this.tanggalSettlement = tanggalSettlement;
+    }
+
+    public Integer getWaktuSettlement() {
+        return this.waktuSettlement;
+    }
+
+    public void setWaktuSettlement(Integer waktuSettlement) {
+        this.waktuSettlement = waktuSettlement;
+    }
+
+    public String getStatus() {
+        return this.status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
 }
