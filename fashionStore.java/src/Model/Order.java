7a405405b8433c990464f19db56c4c5228a2a777
lolkimbek <134467017+lolkimbek@users.noreply.github.com>
2023-05-31 11:01:58 +0700

    attibut
    atribut

--- fashionStore.java/src/Model/Order.java
+++ fashionStore.java/src/Model/Order.java
@@ -13,4 +13,79 @@ public class Order {
      * Jumlah          : Jumlah produk yang dipesan 
      * Id pembayaran   : Id pemabayaran dari suatru produk
      */
+    String nama;
+    String idProduk;
+    String stok;
+    String idPembayaran;
+    int tanggalPesanan;
+    int nomorTelepon;
+    int harga;
+    int jumlah;
+    
+
+    public String getNama() {
+        return this.nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getIdProduk() {
+        return this.idProduk;
+    }
+
+    public void setIdProduk(String idProduk) {
+        this.idProduk = idProduk;
+    }
+
+    public String getStok() {
+        return this.stok;
+    }
+
+    public void setStok(String stok) {
+        this.stok = stok;
+    }
+
+    public String getIdPembayaran() {
+        return this.idPembayaran;
+    }
+
+    public void setIdPembayaran(String idPembayaran) {
+        this.idPembayaran = idPembayaran;
+    }
+
+    public int getTanggalPesanan() {
+        return this.tanggalPesanan;
+    }
+
+    public void setTanggalPesanan(int tanggalPesanan) {
+        this.tanggalPesanan = tanggalPesanan;
+    }
+
+    public int getNomorTelepon() {
+        return this.nomorTelepon;
+    }
+
+    public void setNomorTelepon(int nomorTelepon) {
+        this.nomorTelepon = nomorTelepon;
+    }
+
+    public int getHarga() {
+        return this.harga;
+    }
+
+    public void setHarga(int harga) {
+        this.harga = harga;
+    }
+
+    public int getJumlah() {
+        return this.jumlah;
+    }
+
+    public void setJumlah(int jumlah) {
+        this.jumlah = jumlah;
+    }
+
+
 }
