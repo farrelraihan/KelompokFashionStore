3714880119d63c2950d722c1549fc811e8b1862e
Davin Lim <davin.lim14@gmail.com>
2023-05-31 11:02:57 +0700

    Update Costumer.java

--- fashionStore.java/src/model/Costumer.java
+++ fashionStore.java/src/model/Costumer.java
@@ -15,5 +15,45 @@ public class Costumer {
      * Task
      * Costumer bisa membuat order
      */
+
+     String name;
+     String phoneNumber;
+     String email;
+     String address;
+
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return this.phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+
     
 }
