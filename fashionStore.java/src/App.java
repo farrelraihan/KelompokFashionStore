d4aed0e2664784a96af90547daee6c9238388944
farrelraihan <113604965+farrelraihan@users.noreply.github.com>
2023-06-05 11:10:50 +0700

    Add Init Dummy Data
    Contoh Pertama

--- fashionStore.java/src/App.java
+++ fashionStore.java/src/App.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import model.Produk;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
@@ -46,6 +47,10 @@ public class App {
                 
                 scanner.close();
             }
+
+            public static void init(){
+                Produk Sepatu = new Produk("Adidas", "Ultraboost", "69BCA20", "Sepatu", 870000, 52, "White", "44");
+                
         }
         
     
