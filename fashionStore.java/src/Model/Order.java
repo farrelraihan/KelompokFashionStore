df00c3557d8da3923ee043c02c594e1f34190042
farrelraihan <113604965+farrelraihan@users.noreply.github.com>
2023-07-03 10:07:06 +0700

    Update Order.java
    penyesuaian dengan StarUML

--- fashionStore.java/src/Model/Order.java
+++ fashionStore.java/src/Model/Order.java
@@ -14,60 +14,57 @@ public class Order {
      * Jumlah          : Jumlah produk yang dipesan 
      * Id pembayaran   : Id pemabayaran dari suatru produk
      */
-    String nama;
-    String idProduk;    
-    int stok;
-    String idPembayaran;
-    String tanggalPesanan;
-    String nomorTelepon;
-    int harga;
-    int jumlah;
+    // String nama;
+    // String idProduk;    
+    // int stok;
+    // String idPembayaran;
+    // String tanggalPesanan;
+    // String nomorTelepon;
+    // int harga;
+    // int jumlah;
 
-    public Order(String nama,String idProduk, int stok, String idPembayaran,String tanggalPesanan,String nomorTelepon,int harga,int jumlah){
-        
-        this.nama = nama;
-        this.idProduk = idProduk;
-        this.stok = stok;
-        this.idPembayaran = idPembayaran;
-        this.tanggalPesanan = tanggalPesanan;
-        this.nomorTelepon = nomorTelepon;
-        this.harga = harga;
-        this.jumlah = jumlah;
-    }
+    String orderID;
+    String customerName; //ni ambil dari Customer.java
+    String customerPhoneNumber; //ni ambil dari Customer.java
+    String tanggalPesanan; 
+    int hargaTotal; 
+    String karyawan; 
+
+
     public Order() {
-        
     }
 
-    public String getNama() {
-        return this.nama;
+    public Order(String orderID, String customerName, String customerPhoneNumber, String tanggalPesanan, int hargaTotal, String karyawan) {
+        this.orderID = orderID;
+        this.customerName = customerName;
+        this.customerPhoneNumber = customerPhoneNumber;
+        this.tanggalPesanan = tanggalPesanan;
+        this.hargaTotal = hargaTotal;
+        this.karyawan = karyawan;
     }
 
-    public void setNama(String nama) {
-        this.nama = nama;
+    public String getOrderID() {
+        return this.orderID;
     }
 
-    public String getIdProduk() {
-        return this.idProduk;
+    public void setOrderID(String orderID) {
+        this.orderID = orderID;
     }
 
-    public void setIdProduk(String idProduk) {
-        this.idProduk = idProduk;
+    public String getCustomerName() {
+        return this.customerName;
     }
 
-    public int getStok() {
-        return this.stok;
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
     }
 
-    public void setStok(int stok) {
-        this.stok = stok;
+    public String getCustomerPhoneNumber() {
+        return this.customerPhoneNumber;
     }
 
-    public String getIdPembayaran() {
-        return this.idPembayaran;
-    }
-
-    public void setIdPembayaran(String idPembayaran) {
-        this.idPembayaran = idPembayaran;
+    public void setCustomerPhoneNumber(String customerPhoneNumber) {
+        this.customerPhoneNumber = customerPhoneNumber;
     }
 
     public String getTanggalPesanan() {
@@ -78,43 +75,128 @@ public class Order {
         this.tanggalPesanan = tanggalPesanan;
     }
 
-    public String getNomorTelepon() {
-        return this.nomorTelepon;
+    public int getHargaTotal() {
+        return this.hargaTotal;
     }
 
-    public void setNomorTelepon (String nomorTelepon) {
-        this.nomorTelepon = nomorTelepon;
+    public void setHargaTotal(int hargaTotal) {
+        this.hargaTotal = hargaTotal;
     }
 
-    public int getHarga() {
-        return this.harga;
+    public String getKaryawan() {
+        return this.karyawan;
     }
 
-    public void setHarga(int harga) {
-        this.harga = harga;
-    }
-
-    public int getJumlah() {
-        return this.jumlah;
-    }
-
-    public void setJumlah(int jumlah) {
-        this.jumlah = jumlah;
+    public void setKaryawan(String karyawan) {
+        this.karyawan = karyawan;
     }
 
     @Override
     public String toString() {
-        return "|" +
-            " " + getNama() + "\t|" +
-            " " + getIdProduk() + "\t|" +
-            " " + getStok() + "\t|" +
-            " " + getIdPembayaran() + "\t|" +
-            " " + getTanggalPesanan() + "\t|" +
-            " " + getNomorTelepon() + "\t|" +
-            " " + getHarga() + "\t|" +
-            " '" + getJumlah() + "\t|" +
-            "";
+        return "{" +
+            " orderID='" + getOrderID() + "'" +
+            ", customerName='" + getCustomerName() + "'" +
+            ", customerPhoneNumber='" + getCustomerPhoneNumber() + "'" +
+            ", tanggalPesanan='" + getTanggalPesanan() + "'" +
+            ", hargaTotal='" + getHargaTotal() + "'" +
+            ", karyawan='" + getKaryawan() + "'" +
+            "}";
     }
+
+    
+
+    // public Order(String nama,String idProduk, int stok, String idPembayaran,String tanggalPesanan,String nomorTelepon,int harga,int jumlah){
+        
+    //     this.nama = nama;
+    //     this.idProduk = idProduk;
+    //     this.stok = stok;
+    //     this.idPembayaran = idPembayaran;
+    //     this.tanggalPesanan = tanggalPesanan;
+    //     this.nomorTelepon = nomorTelepon;
+    //     this.harga = harga;
+    //     this.jumlah = jumlah;
+    // }
+    // public Order() {
+        
+    // }
+
+    // public String getNama() {
+    //     return this.nama;
+    // }
+
+    // public void setNama(String nama) {
+    //     this.nama = nama;
+    // }
+
+    // public String getIdProduk() {
+    //     return this.idProduk;
+    // }
+
+    // public void setIdProduk(String idProduk) {
+    //     this.idProduk = idProduk;
+    // }
+
+    // public int getStok() {
+    //     return this.stok;
+    // }
+
+    // public void setStok(int stok) {
+    //     this.stok = stok;
+    // }
+
+    // public String getIdPembayaran() {
+    //     return this.idPembayaran;
+    // }
+
+    // public void setIdPembayaran(String idPembayaran) {
+    //     this.idPembayaran = idPembayaran;
+    // }
+
+    // public String getTanggalPesanan() {
+    //     return this.tanggalPesanan;
+    // }
+
+    // public void setTanggalPesanan(String tanggalPesanan) {
+    //     this.tanggalPesanan = tanggalPesanan;
+    // }
+
+    // public String getNomorTelepon() {
+    //     return this.nomorTelepon;
+    // }
+
+    // public void setNomorTelepon (String nomorTelepon) {
+    //     this.nomorTelepon = nomorTelepon;
+    // }
+
+    // public int getHarga() {
+    //     return this.harga;
+    // }
+
+    // public void setHarga(int harga) {
+    //     this.harga = harga;
+    // }
+
+    // public int getJumlah() {
+    //     return this.jumlah;
+    // }
+
+    // public void setJumlah(int jumlah) {
+    //     this.jumlah = jumlah;
+    // }
+
+    // @Override
+    // public String toString() {
+    //     return "|" +
+    //         " " + getNama() + "\t|" +
+    //         " " + getIdProduk() + "\t|" +
+    //         " " + getStok() + "\t|" +
+    //         " " + getIdPembayaran() + "\t|" +
+    //         " " + getTanggalPesanan() + "\t|" +
+    //         " " + getNomorTelepon() + "\t|" +
+    //         " " + getHarga() + "\t|" +
+    //         " '" + getJumlah() + "\t|" +
+    //         "";
+    // }
 
 
 
