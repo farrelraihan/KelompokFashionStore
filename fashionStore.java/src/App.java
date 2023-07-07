7e2389c9bab28a3ac43f60b72df06f3aa76c10c5
owenloe <88763692+owenloe@users.noreply.github.com>
2023-07-07 12:46:28 +0700

    Commit Revised Functions for addCustomer , viewCustomer , addOrder, viewOrder
    Commit Revised Functions for addCustomer , viewCustomer , addOrder, viewOrder

--- fashionStore.java/src/App.java
+++ fashionStore.java/src/App.java
@@ -131,3 +131,200 @@ public static void init(){
                 karyawan Dandy = new karyawan("03081220033", "Dandy", "Manajer", "Produksi", 8500000, "081260989119");
 
             }
+
+public static void addCustomer() {
+        Scanner scanner = new Scanner(System.in);
+
+        try{
+        System.out.print("Enter customer name: ");
+        String name = scanner.nextLine();
+        System.out.print("Enter customer phone number: ");
+        String phoneNumber = scanner.nextLine();
+        System.out.print("Enter customer email: ");
+        String email = scanner.nextLine();
+        System.out.print("Enter customer address: ");
+        String address = scanner.nextLine();
+
+        // Create a new Customer object and add it to the customerList ArrayList
+        Customer customer = new Customer(name, phoneNumber, email, address);
+        customerList.add(customer);
+        System.out.println("Customer added successfully!");
+    } catch (NoSuchElementException e){
+        System.out.println("Error occurred while adding customer: " + e.getMessage());
+    }
+}
+    public static void viewCustomer() {
+        if (customerList.isEmpty()) {
+            System.out.println("No Customer found.");
+        } else {
+            System.out.println("--------------------------------------------------------");
+            System.out.println("Available Customers:");
+            System.out.println("--------------------------------------------------------");
+            System.out.printf("| %-5s | %-20s | %-15s |\n", "No", "Customer Name", "Phone Number");
+    
+            for (int i = 0; i < customerList.size(); i++) {
+            Customer customer = customerList.get(i);
+            System.out.printf("| %-5d | %-20s | %-15s |\n", (i + 1), customer.getCustomerName(), customer.getCustomerPhoneNumber());
+            }
+    
+        System.out.println("--------------------------------------------------------");}
+    }
+
+
+    public static void addOrder() { //try fix  original nothing inside
+    //Scanner scanner = new Scanner(System.in); //try fix
+
+    LocalTime localTime = LocalTime.now();
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+    String tanggalPesanan = localTime.format(formatter);
+    int index = orderList.size() + 1;
+
+    String orderID = "OR" + String.format("%03d", index); // Assuming a maximum of 999 Orders
+
+    viewCustomer();
+
+    // Prompt for customer selection
+    System.out.print("Select a customer: ");
+    int selectedCustomerIndex;
+    
+    try {
+    //if (scanner.hasNextInt()) {
+        selectedCustomerIndex = scanner.nextInt();
+    } catch (InputMismatchException e) {
+        System.out.println("Invalid input. Please enter a valid integer.");
+        scanner.nextLine();
+        return;
+    }
+
+    if (selectedCustomerIndex < 1 || selectedCustomerIndex > customerList.size()) {
+        System.out.println("Invalid customer selection.");
+        scanner.close();
+        return;
+    }
+
+    Customer selectedCustomer = customerList.get(selectedCustomerIndex - 1);
+    String customerName = selectedCustomer.getCustomerName();
+    String customerPhoneNumber = selectedCustomer.getCustomerPhoneNumber();
+
+    viewKaryawan();
+
+    scanner.nextLine(); // Consume the newline character after reading the integer
+
+    System.out.print("Enter the ID of the karyawan: ");
+    String selectedKaryawanID = scanner.nextLine(); //TRY FIX
+    //scanner.nextLine(); //trying fix add nextline convert up to next
+    String karyawanName = " ";
+
+    // Find the karyawan in the karyawanList based on the ID
+    Karyawan selectedKaryawan = null;
+    for (Karyawan karyawan : karyawanList) {
+        if (karyawan.getIdKaryawan().equals(selectedKaryawanID)) {
+            selectedKaryawan = karyawan;
+            break;
+        }
+    }
+
+    if (selectedKaryawan != null) {
+        // Retrieve selected karyawan data
+        karyawanName = selectedKaryawan.getNamaKaryawan();
+    } else {
+        System.out.println("Invalid karyawan ID.");
+        //scanner.close();
+        return;
+    }
+    
+    //scanner.nextLine(); //fix error //edit: doesnt work.
+    Integer hargaTotal = null;
+
+    Order order = new Order(orderID, customerName, customerPhoneNumber, tanggalPesanan, hargaTotal, karyawanName);
+
+    order.setOrderID(orderID);
+    order.setKaryawan(karyawanName);
+    order.setDetailOrderList(new ArrayList<>()); //trying fix latest
+    // Add the order to the orderList
+    orderList.add(order);
+    System.out.println("Order added successfully!");
+
+   // scanner.close();
+}
+
+public static void viewOrder() {
+    if (orderList.isEmpty()) {
+        System.out.println("No Order found.");
+    } else {
+        System.out.println("--------------------------------------------------------");
+        System.out.println("Available Orders:");
+        System.out.println("--------------------------------------------------------");
+        System.out.printf("| %-8s | %-20s | %-15s | %-15s | %-15s | %-12s |\n",
+                "Order ID", "Customer Name", "Phone Number", "Tanggal Pesanan", "Karyawan", "Total Price");
+
+        for (Order order : orderList) {
+            System.out.printf("| %-8s | %-20s | %-15s | %-15s | %-15s | %-12d |\n",
+                    order.getOrderID(), order.getCustomerName(),
+                    order.getCustomerPhoneNumber(), order.getTanggalPesanan(),
+                    order.getKaryawan(), order.getHargaTotal());
+        }
+
+        System.out.println("--------------------------------------------------------");
+    }
+}
+
+
+
+
+    public static void addProduk() {
+    Scanner scanner = new Scanner(System.in);
+    
+    try{
+    System.out.print("Enter product brand: ");
+    String merkProduk = scanner.nextLine();
+    System.out.print("Enter product name: ");
+    String namaProduk = scanner.nextLine();
+    System.out.print("Enter product category: ");
+    String kategoriProduk = scanner.nextLine();
+    System.out.print("Enter product price: ");
+    int hargaProduk = scanner.nextInt();
+    System.out.print("Enter product stock: ");
+    int stokProduk = scanner.nextInt();
+    scanner.nextLine(); // Consume the newline character
+    System.out.print("Enter product color: ");
+    String warnaProduk = scanner.nextLine();
+    System.out.print("Enter product size: ");
+    String ukuranProduk = scanner.nextLine();
+
+    // Generate the Produk ID based on the index of the ArrayList
+    int index = produkList.size() + 1;
+    String idProduk = "PK" + String.format("%03d", index); // Assuming a maximum of 999 Produk
+    
+
+    // Create a new Produk object and set its properties
+    Produk produk = new Produk(idProduk, merkProduk, namaProduk, kategoriProduk, hargaProduk, stokProduk, warnaProduk, ukuranProduk);
+    produk.setIdProduk(idProduk);
+    // Add the produk to the ArrayList
+    produkList.add(produk);
+    
+    System.out.println("Produk added successfully!");
+ } catch (Exception e){
+    System.out.println("Error occurred while adding product: " + e.getMessage());
+    }
+}
+
+public static void viewProduk() {
+    if (produkList.isEmpty()) {
+        System.out.println("No Produk found.");
+    } else {
+        System.out.println("Produk List:");
+        System.out.println("--------------------------------------------------------");
+        System.out.printf("| %-5s | %-10s | %-20s | %-15s | %-15s | %-10s | %-10s |\n",
+                "ID", "Brand", "Nama Produk", "Harga Satuan", "Stok Produk", "Warna", "Ukuran");
+
+        for (Produk produk : produkList) {
+            System.out.printf("| %-5s | %-10s | %-20s | %-15d | %-15d | %-10s | %-10s |\n",
+                    produk.getIdProduk(), produk.getMerkProduk(), produk.getNamaProduk(),
+                    produk.getHargaProduk(), produk.getStokProduk(), produk.getWarnaProduk(),
+                    produk.getUkuranProduk());
+        }
+
+        System.out.println("--------------------------------------------------------");
+    }
+}
